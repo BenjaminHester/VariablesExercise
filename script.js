41b18d4385c9e9e5021fb98ca957d6d52de4cb85
Benjamin Hester <benjamin.hester@stu.barren.kyschools.us>
2024-01-25 10:57:10 -0600

    inserted correct answers as comments for incorrect answers.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let num = 20;
 num = 15;
 num += 7;
 num - 1;
+// num --
 num = "this is not a number";
 let blank;
 console.log(blank);
@@ -13,3 +14,4 @@ const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01
 console.log(quarter, dime, nickel, penny);
 let letters = "qwertyuiop"
 console.log("qwertyuiop"[9]);
+// console.log(letters[letters.length - 1]);
